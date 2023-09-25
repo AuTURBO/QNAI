78e15abab82b00410bc86d3b36b235bd13c0ca3a
jinwon kim <mqjinwon@gmail.com>
2023-09-25 22:56:18 +0900

    change python code to fit google style guide

--- isaac_ws/utils/omnigraph.py
+++ isaac_ws/utils/omnigraph.py
@@ -5,6 +5,7 @@ creator: Jinwon Kim
 
 import omni.graph.core as og
 
+
 class omnigraph_helper():
     """
     This class is for generating omnigraph.
@@ -22,29 +23,36 @@ class omnigraph_helper():
             self._ros_version = "ROS1"
             self._ros_bridge_version = "ros_bridge."
         pass
-    
+
     def ros_clock(self):
         try:
             (self._clock_graph, _, _, _) = og.Controller.edit(
                 {
-                    "graph_path": "/ROS_Clock",
-                    "evaluator_name": "execution",
-                    "pipeline_stage": og.GraphPipelineStage.GRAPH_PIPELINE_STAGE_SIMULATION,
+                    "graph_path":
+                        "/ROS_Clock",
+                    "evaluator_name":
+                        "execution",
+                    "pipeline_stage":
+                        og.GraphPipelineStage.GRAPH_PIPELINE_STAGE_SIMULATION,
                 },
                 {
                     self._keys.CREATE_NODES: [
                         ("OnTick", "omni.graph.action.OnTick"),
-                        ("readSimTime", "omni.isaac.core_nodes.IsaacReadSimulationTime"),
-                        ("publishClock", "omni.isaac." + self._ros_bridge_version + self._ros_version + "PublishClock"),
+                        ("readSimTime",
+                         "omni.isaac.core_nodes.IsaacReadSimulationTime"),
+                        ("publishClock",
+                         "omni.isaac." + self._ros_bridge_version +
+                         self._ros_version + "PublishClock"),
                     ],
                     self._keys.CONNECT: [
                         ("OnTick.outputs:tick", "publishClock.inputs:execIn"),
-                        ("readSimTime.outputs:simulationTime", "publishClock.inputs:timeStamp"),
+                        ("readSimTime.outputs:simulationTime",
+                         "publishClock.inputs:timeStamp"),
                     ],
                 },
             )
             return True
-        
+
         except Exception as e:
             print(e)
 
@@ -54,9 +62,12 @@ class omnigraph_helper():
         try:
             (self._on_tick, _, _, _) = og.Controller.edit(
                 {
-                    "graph_path": "/ROS_IMU",
-                    "evaluator_name": "execution",
-                    "pipeline_stage": og.GraphPipelineStage.GRAPH_PIPELINE_STAGE_SIMULATION,
+                    "graph_path":
+                        "/ROS_IMU",
+                    "evaluator_name":
+                        "execution",
+                    "pipeline_stage":
+                        og.GraphPipelineStage.GRAPH_PIPELINE_STAGE_SIMULATION,
                 },
                 {
                     self._keys.CREATE_NODES: [
@@ -66,7 +77,7 @@ class omnigraph_helper():
                 },
             )
             return True
-        
+
         except Exception as e:
             print(e)
 
