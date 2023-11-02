443ec8c565a9ea516401c98c2d1577bfa0805394
Jinwon Kim <jwkim@ghostrobotics.co.kr>
2023-11-02 20:11:10 +0900

    Add anymal functions

--- isaac_ws/anymal_standalone.py
+++ isaac_ws/anymal_standalone.py
@@ -1,3 +1,7 @@
+"""
+This file is anymal standalone python file.
+"""
+
 # Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
 #
 # NVIDIA CORPORATION and its licensors retain all intellectual property
@@ -12,10 +16,9 @@ from omni.isaac.kit import SimulationApp
 simulation_app = SimulationApp({"headless": False})
 
 from omni.isaac.core import World
-from omni.isaac.quadruped.robots import Anymal
-from omni.isaac.core.utils.prims import define_prim, get_prim_at_path
+from utils.anymal import Anymal
+from omni.isaac.core.utils.prims import define_prim, get_prim_at_path  # pylint: disable=ungrouped-imports
 from omni.isaac.core.utils.nucleus import get_assets_root_path
-from pxr import Gf, UsdGeom
 
 import omni.appwindow  # Contains handle to keyboard
 import numpy as np
@@ -25,7 +28,10 @@ import sys
 import os
 
 
-class Anymal_runner(object):
+class AnymalRunner(object):
+    """[summary]
+    Main class to run the simulation
+    """
 
     def __init__(self, physics_dt, render_dt) -> None:
         """
@@ -36,11 +42,9 @@ class Anymal_runner(object):
         Argument:
         physics_dt {float} -- Physics downtime of the scene.
         render_dt {float} -- Render downtime of the scene.
-        
+
         """
-        self._world = World(stage_units_in_meters=1.0,
-                            physics_dt=physics_dt,
-                            rendering_dt=render_dt)
+        self._world = World(stage_units_in_meters=1.0, physics_dt=physics_dt, rendering_dt=render_dt)
 
         assets_root_path = get_assets_root_path()
         if assets_root_path is None:
@@ -60,13 +64,11 @@ class Anymal_runner(object):
         prim = get_prim_at_path("/World/hospital")
         if not prim.IsValid():
             prim = define_prim("/World/hospital", "Xform")
-            env_asset_path = os.path.join(assets_root_path,
-                                          "Assets/Envs/hospital.usd")
+            env_asset_path = os.path.join(assets_root_path, "Assets/Envs/hospital.usd")
             print(env_asset_path)
             prim.GetReferences().AddReference(env_asset_path)
 
-        robot_usd_path = os.path.join(assets_root_path,
-                                      "Assets/Robots/anymal_c.usd")
+        robot_usd_path = os.path.join(assets_root_path, "Assets/Robots/anymal_c.usd")
 
         self._anymal = self._world.scene.add(
             Anymal(
@@ -74,7 +76,8 @@ class Anymal_runner(object):
                 name="Anymal",
                 usd_path=robot_usd_path,
                 position=np.array([0, 0, 0.70]),
-            ))
+            )
+        )
 
         self._world.reset()
         self._enter_toggled = 0
@@ -108,22 +111,20 @@ class Anymal_runner(object):
         [Summary]
 
         Set up keyboard listener and add physics callback
-        
+
         """
         self._appwindow = omni.appwindow.get_default_app_window()
         self._input = carb.input.acquire_input_interface()
         self._keyboard = self._appwindow.get_keyboard()
-        self._sub_keyboard = self._input.subscribe_to_keyboard_events(
-            self._keyboard, self._sub_keyboard_event)
-        self._world.add_physics_callback("anymal_advance",
-                                         callback_fn=self.on_physics_step)
+        self._sub_keyboard = self._input.subscribe_to_keyboard_events(self._keyboard, self._sub_keyboard_event)
+        self._world.add_physics_callback("anymal_advance", callback_fn=self.on_physics_step)
 
     def on_physics_step(self, step_size) -> None:
         """
         [Summary]
 
         Physics call back, switch robot mode and call robot advance function to compute and apply joint torque
-        
+
         """
         if self.needs_reset:
             self._world.reset(True)
@@ -135,21 +136,20 @@ class Anymal_runner(object):
         [Summary]
 
         Step simulation based on rendering downtime
-        
+
         """
         # change to sim running
         while simulation_app.is_running():
             self._world.step(render=True)
             if not self._world.is_simulating():
                 self.needs_reset = True
-        return
 
-    def _sub_keyboard_event(self, event, *args, **kwargs) -> bool:
+    def _sub_keyboard_event(self, event) -> bool:
         """
         [Summary]
 
         Keyboard subscriber callback to when kit is updated.
-        
+
         """
         # reset event
         self._event_flag = False
@@ -157,14 +157,12 @@ class Anymal_runner(object):
         if event.type == carb.input.KeyboardEventType.KEY_PRESS:
             # on pressing, the command is incremented
             if event.input.name in self._input_keyboard_mapping:
-                self._base_command[0:3] += np.array(
-                    self._input_keyboard_mapping[event.input.name])
+                self._base_command[0:3] += np.array(self._input_keyboard_mapping[event.input.name])
 
         elif event.type == carb.input.KeyboardEventType.KEY_RELEASE:
             # on release, the command is decremented
             if event.input.name in self._input_keyboard_mapping:
-                self._base_command[0:3] -= np.array(
-                    self._input_keyboard_mapping[event.input.name])
+                self._base_command[0:3] -= np.array(self._input_keyboard_mapping[event.input.name])
         return True
 
 
@@ -173,18 +171,18 @@ def main():
     [Summary]
 
     Parse arguments and instantiate the ANYmal runner
-    
+
     """
     physics_dt = 1 / 200.0
     render_dt = 1 / 60.0
 
-    runner = Anymal_runner(physics_dt=physics_dt, render_dt=render_dt)
+    runner = AnymalRunner(physics_dt=physics_dt, render_dt=render_dt)
     simulation_app.update()
     runner.setup()
 
     # an extra reset is needed to register
-    runner._world.reset()
-    runner._world.reset()
+    runner._world.reset()  # pylint: disable=protected-access
+    runner._world.reset()  # pylint: disable=protected-access
     runner.run()
     simulation_app.close()
 
